77036d604c4ba375bfa055ddbb6777460f5c2f18
aupadhyay77 <57941236+aupadhyay77@users.noreply.github.com>
2020-06-15 07:29:51 +0530

    react test

---
+++ precache-manifest.51965a3650676b0300dbc3c48848f7e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57645a2703454deffaad86cefa174b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab50ab626912b56e5bc4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5153b06881fdf866dc97",
+    "url": "/static/js/2.d0f229c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0f229c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab50ab626912b56e5bc4",
+    "url": "/static/js/main.063afb6d.chunk.js"
+  },
+  {
+    "revision": "3b8ba9a0619264760848",
+    "url": "/static/js/runtime-main.cfabfa32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
